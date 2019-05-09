8b4323ee6f3693328c07b9ef2b5c9aaed0612887
takayukioda <takayuki@odashi.me>
2019-05-09 20:14:47 +0900

    Update tomcat.sql
    - upper case
    - if exists
    - formatting

--- sql/tomcat.sql
+++ sql/tomcat.sql
@@ -1,9 +1,17 @@
-create database company_db;
-use company_db;
-create table shain_table(id smallint UNIQUE, name text, sei text, nen smallint, address text);
-insert into shain_table(id,name,sei,nen,address) values(200,'R‘¾˜Y','’j',2002,'“Œ‹“s¢“c’J‹æ');
-insert into shain_table(id,name,sei,nen,address) values(201,'—é–ØM','’j',2003,'‹{éŒ§å‘äs');
-insert into shain_table(id,name,sei,nen,address) values(202,'“¡D','—',2004,'•Ÿ‰ªŒ§•Ÿ‰ªs');
-insert into shain_table(id,name,sei,nen,address) values(203,'‚‹´”ü','—',2005,'•Ÿ‰ªŒ§•Ÿ‰ªs');
-insert into shain_table(id,name,sei,nen,address) values(204,'²“¡ˆê','’j',2003,'‹{éŒ§å‘äs');
-insert into shain_table(id,name,sei,nen,address) values(205,'—Ñ½“ñ','’j',2002,'“Œ‹“s]“Œ‹æ');
+CREATE DATABASE IF NOT EXISTS company_db;
+USE company_db;
+
+CREATE TABLE IF NOT EXISTS shain_table (
+	id      smallint UNIQUE,
+	name    text,
+	sei     text,
+	nen     smallint,
+	address text
+);
+
+INSERT INTO shain_table(id, name, sei, nen, address) VALUES (100, 'RŠİ‘¾˜Y',   '’j', 2002, '“Œ‹“s¢“c’J‹æ');
+INSERT INTO shain_table(id, name, sei, nen, address) VALUES (101, '—é–ØMm',   '’j', 2003, '‹{éŒ§å‘äs');
+INSERT INTO shain_table(id, name, sei, nen, address) VALUES (102, '“¡ˆäD',   '—', 2004, '•Ÿ‰ªŒ§•Ÿ‰ªs');
+INSERT INTO shain_table(id, name, sei, nen, address) VALUES (103, '‚‹´‰F²”ü', '—', 2005, '•Ÿ‰ªŒ§•Ÿ‰ªs');
+INSERT INTO shain_table(id, name, sei, nen, address) VALUES (104, '²“¡ˆê“ñO', '’j', 2003, '‹{éŒ§å‘äs');
+INSERT INTO shain_table(id, name, sei, nen, address) VALUES (105, '•½—Ñ½“ñ',   '’j', 2002, '“Œ‹“s]“Œ‹æ');
