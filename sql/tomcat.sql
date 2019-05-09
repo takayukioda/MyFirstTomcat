ba0004ede93a94364f083471acb49216578fb194
takayukioda <takayuki@odashi.me>
2019-05-09 20:03:22 +0900

    Create tomcat.sql

---
+++ sql/tomcat.sql
@@ -0,0 +1,9 @@
+create database company_db;
+use company_db;
+create table shain_table(id smallint UNIQUE, name text, sei text, nen smallint, address text);
+insert into shain_table(id,name,sei,nen,address) values(200,'R‘¾˜Y','’j',2002,'“Œ‹“s¢“c’J‹æ');
+insert into shain_table(id,name,sei,nen,address) values(201,'—é–ØM','’j',2003,'‹{éŒ§å‘äs');
+insert into shain_table(id,name,sei,nen,address) values(202,'“¡D','—',2004,'•Ÿ‰ªŒ§•Ÿ‰ªs');
+insert into shain_table(id,name,sei,nen,address) values(203,'‚‹´”ü','—',2005,'•Ÿ‰ªŒ§•Ÿ‰ªs');
+insert into shain_table(id,name,sei,nen,address) values(204,'²“¡ˆê','’j',2003,'‹{éŒ§å‘äs');
+insert into shain_table(id,name,sei,nen,address) values(205,'—Ñ½“ñ','’j',2002,'“Œ‹“s]“Œ‹æ');
