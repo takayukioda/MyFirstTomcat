05f37ae8922754b09da2ab68416214d24d8f769e
takayukioda <takayuki@odashi.me>
2019-05-12 15:26:06 +0900

    Update setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+# Unfortunently, this script is not for automation
+# Since some commands are build as interfactive command such as `mysql_secure_installation`
+
 [ "$(whoami)" != "root" ] && echo "Run this script as root" >&2 && exit 1
 
 # Install all required softwares
-amazon-linux-extras install tomcat8.5
+amazon-linux-extras install -y tomcat8.5
 yum update -q -y
-yum install -q -y
+yum install -q -y \
     git \
     java-1.8.0-openjdk \
     maven \
     mariadb-server \
     httpd
 
-cat /etc/my.cnf.d/encoding.cnf <<EOF
+cat > /etc/my.cnf.d/encoding.cnf <<EOF
 [mysqld]
 character-set-server=utf8mb4
 
@@ -21,4 +24,5 @@ default-character-set=cp932
 EOF
 
 systemctl start mariadb
-mysql_secure_installation --use-default
+# sad, but it's interactive
+mysql_secure_installation
